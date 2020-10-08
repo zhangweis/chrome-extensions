f25fb6e66c002f6ab67b194e362b9da63ab4f3b8
cdzhangw <cdzhangw@gmail.com>
2020-10-08 19:38:32 +0800

    fetches and results.

--- README.md
+++ README.md
@@ -7,33 +7,38 @@
  
  This is to support multiple options. You can store multiple options and use jq filter to select which one to use.
 
- The result object contains below fields:
+ The result object {fetches, periodSeconds, displaySeconds} fetches field is an array which contains below fields:
 
  urls: JSON Array where ${timestamp} will be replaced with current timestamp. 
  
  jq: which takes from urls fetch results as an array and results in {badge, content}, and badge will be shown in extension's badge and content will be shown as html in popup by https://www.npmjs.com/package/tableify.
- 
+
+--
  periodSeconds: controls period in seconds.
+ displaySeconds: controls display period in seconds. This is to support multiple results.
 
  Below is my example of watching otc price and funding_time (currently selected) of okex.
 
 Options(urls, jq, periodSeconds): 
+{
+"fetches":
 [{
 "urls":
 ["https://www.okex.me/v3/c2c/tradingOrders/book?t=${timestamp}&quoteCurrency=CNY&baseCurrency=USDT&side=sell&paymentMethod=all&userType=all"]
 ,"jq":
 ".[0].data.sell|map(select(.quoteMinAmountPerOrder|tonumber<10000))|map({price, amount:.quoteMinAmountPerOrder,name:.creator.nickName})|{badge:.[0].price,content:.}"
-,"periodSeconds":
-60
 },
 {
 "urls":
 ["https://www.okex.com/api/swap/v3/instruments/BSV-USD-SWAP/funding_time"]
 ,"jq":
 ".[0]|{badge:(.estimated_rate|tonumber*10000|tostring[:4]),content:.}"
+}]
 ,"periodSeconds":
 60
-}]
+,"displaySeconds":
+10
+}
 
 jq(filter which option to use):
-.[1]
+.
--- watchjson/background.js
+++ watchjson/background.js
@@ -1,37 +1,65 @@
 var timer;
+var displayTimer;
+
 function start() {
-  chrome.storage.sync.get(['settings'], async ({settings})=>{
-    var {options,jq:jqFilter} = settings;
-    const optionsObj = jq.json(JSON.parse(options), jqFilter);
-    loadJson();
-timer = setInterval(loadJson, Number(optionsObj.periodSeconds)*1000);
+    chrome.storage.sync.get(['settings'], async ({ settings }) => {
+        var { options, jq: jqFilter } = settings;
+        var optionsObj = jq.json(jq.json({}, options), jqFilter);
+        loadJson();
+        clearInterval(timer);
+        timer = setInterval(loadJson, Number(optionsObj.periodSeconds || "60") * 1000);
     });
 }
 start();
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if(["option changed","reload"].indexOf(request.type)>=0) {
-            clearInterval(timer);
-            start();
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (["option changed", "reload"].indexOf(request.type) >= 0) {
+        start();
+    }
+})
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if (namespace != 'local') return;
+    for (var key in changes) {
+        if (key === 'result') {
+            chrome.browserAction.setBadgeText({ text: '' + changes[key].newValue.badge });
         }
-    })
-    
+        if (key === 'results') {
+            var results = changes[key].newValue;
+            var length = results.length;
+            var i = 0;
+            chrome.storage.sync.get(['settings'], async ({ settings }) => {
+                var { options, jq: jqFilter } = settings;
+                var optionsObj = jq.json(jq.json({}, options), jqFilter);
+                clearInterval(displayTimer);
+                displayTimer = setInterval(async () => {
+                    chrome.storage.local.set({ result: results[i++] });
+                    if (i >= length) i = 0;
+                }, (optionsObj.displaySeconds || 5) * 1000);
+            });
+        }
+    }
+
+});
+
+
 async function loadJson() {
-//    var url = 'https://www.okex.me/v3/c2c/tradingOrders/book?t=${timestamp}&quoteCurrency=CNY&baseCurrency=USDT&side=sell&paymentMethod=all&userType=all';
-    chrome.storage.sync.get(['settings'], async ({settings})=>{
-        var {options,jq:jqFilter} = settings;
-        var optionsObj = jq.json(JSON.parse(options), jqFilter);
-        const {urls, jq:jqPath} = optionsObj;
-        var json = await Promise.all(urls.map(async (urlToGo)=> {
-            urlToGo = urlToGo.replaceAll('${timestamp}',Number(new Date()));
-        console.log('fetching '+urlToGo);
-            var json = await (await fetch(urlToGo)).json();
-            return json;
+    //    var url = 'https://www.okex.me/v3/c2c/tradingOrders/book?t=${timestamp}&quoteCurrency=CNY&baseCurrency=USDT&side=sell&paymentMethod=all&userType=all';
+    chrome.storage.sync.get(['settings'], async ({ settings }) => {
+        var { options, jq: jqFilter } = settings;
+        var optionsObj = jq.json(jq.json({}, options), jqFilter);
+        var result = await Promise.all(optionsObj.fetches.map(async (fetchOption) => {
+            const { urls, jq: jqPath } = fetchOption;
+            var json = await Promise.all(urls.map(async (urlToGo) => {
+                urlToGo = urlToGo.replaceAll('${timestamp}', Number(new Date()));
+                console.log('fetching ' + urlToGo);
+                var json = await (await fetch(urlToGo)).json();
+                return json;
+            }));
+            var result = jq.json(json, jqPath);
+            return result;
         }));
-var result = jq.json(json, jqPath);
-        var info = {result: result};
-        
-    chrome.storage.local.set(info);
-    chrome.browserAction.setBadgeText({text:''+info.result.badge});
+        var info = { results: result };
+
+        chrome.storage.local.set(info);
 
     })
 }
