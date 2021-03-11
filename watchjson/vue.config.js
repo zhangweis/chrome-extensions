ea2a60bb4fefffd79b6268c60fceb478cb508ef7
John <cdzhangw@gmail.com>
2021-03-11 18:03:59 +0800

    remove inline

--- watchjson/vue.config.js
+++ watchjson/vue.config.js
@@ -12,13 +12,13 @@ module.exports = {
     ]
   },
   chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].inlineSource = '.(js|css)$'
-        return args
-      });
-      config.plugin('inlinehtml')
-      .use(HtmlWebpackInlineSourcePlugin);
+    // config
+    //   .plugin('html')
+    //   .tap(args => {
+    //     args[0].inlineSource = '.(js|css)$'
+    //     return args
+    //   });
+    //   config.plugin('inlinehtml')
+    //   .use(HtmlWebpackInlineSourcePlugin);
   }
 };
