87a922c17b33ace403109f05c64f3f3e892b0ff3
John <cdzhangw@gmail.com>
2023-08-31 08:39:42 +0800

    deno format badges

--- watchjson/index-deno.mjs
+++ watchjson/index-deno.mjs
@@ -1,6 +1,7 @@
 import jq from "https://raw.githubusercontent.com/zhangweis/deno-tools/main/jq.asm.bundle.js"
-import {parseFetchAndJq} from "./Controller.mjs";
+import {parseFetchAndJq,formatBadges} from "./Controller.mjs";
 const stdinContent = await Deno.readAll(Deno.stdin);
 const input = new TextDecoder().decode(stdinContent);
 var {result} =await parseFetchAndJq(input,{jq});
+if (result.badge) {result.formattedBadges = formatBadges(result.badge)} 
 console.log(JSON.stringify(result));
