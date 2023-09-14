7f6f58163846a278241602d2ffe8dff0f66c4895
John <cdzhangw@gmail.com>
2023-09-14 11:26:06 +0800

    -k to keep badge

--- watchjson/index-deno.mjs
+++ watchjson/index-deno.mjs
@@ -2,8 +2,10 @@ import jq from "https://raw.githubusercontent.com/zhangweis/deno-tools/main/jq.a
 import {vsprintf,sprintf} from 'https://jspm.dev/sprintf-js';
 import forceArray from "https://jspm.dev/force-array";
 import {parseFetchAndJq,formatBadges} from "./Controller.mjs";
+import { parse } from "https://deno.land/std/flags/mod.ts";
+const args = parse(Deno.args);
 const stdinContent = await Deno.readAll(Deno.stdin);
 const input = new TextDecoder().decode(stdinContent);
 var {result} =await parseFetchAndJq(input,{jq});
-if (result.badge) {result.formattedBadges = formatBadges(result.badge,{vsprintf,sprintf,forceArray});delete result.badge} 
+if (result.badge) {result.formattedBadges = formatBadges(result.badge,{vsprintf,sprintf,forceArray});if (!args.k)delete result.badge} 
 console.log(JSON.stringify(result));
